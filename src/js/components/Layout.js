7c4b93be8e85d45ef9fd80030293da5a27610cf7
ViktorBjornlund <viktor.bjornlund@consid.se>
2017-07-05 16:18:00 +0200

    first commit

---
+++ src/js/components/Layout.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Footer from "./Footer";
+import Header from "./Header";
+
+export default class Layout extends React.Component {
+    constructor() {
+        super();
+        this.state = {title: "Welcome"};
+    }
+    render() {
+        setTimeout(() => {
+            this.setState({title: "Welcome, Viktor."});
+        }, 2000);
+        
+        return (
+            <div>
+                <Header title={this.state.title}/>
+                <Footer />
+            </div>
+        );
+    }
+}
